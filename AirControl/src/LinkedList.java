7c51f622c149d2956f06f614b5786e8d99ab37a8
Bimal Gaudel <bimalgaudel@gmail.com>
2020-04-23 20:17:23 -0400

    Adds LinkedList class

---
+++ AirControl/src/LinkedList.java
@@ -0,0 +1,284 @@
+/**
+ * A linked-list data structure for holding RectangleRecord objects.
+ * 
+ * @param   <E> The type of object stored in the list.
+ * 
+ * @author      Bimal Gaudel
+ * 
+ * @version     Feb 2020
+ */
+public class LinkedList<E extends Comparable<E>> {
+    /** The head node of the list. */
+    private Node headerNode;
+
+    /** The tail node of the list. */
+    private Node trailerNode;
+
+    /** The current node used for iterating. */
+    private Node currNode;
+
+    /** The number of items in the list. */
+    private int count;
+
+    /** Empty parameter LinkedList constructor. */
+    LinkedList() {
+        headerNode = new Node();
+        trailerNode = new Node();
+        currNode = trailerNode;
+        headerNode.setNext(trailerNode);
+        count = 0;
+    }
+
+    /**
+     * Check if the list is empty.
+     * 
+     * @return True if empty, false otherwise.
+     */
+    public boolean isEmpty() {
+        return (headerNode.getNext() == trailerNode);
+    }
+
+    /**
+     * Append data to the list.
+     * 
+     * @param data Data to be appended.
+     */
+    public void append(E data) {
+        moveToEnd();
+        currNode.setData(data);
+        trailerNode = new Node();
+        currNode.setNext(trailerNode);
+        ++count;
+    }
+
+    /**
+     * Insert data in the list at the position that has an existing data is
+     * greater than or equal to the data being inserted.
+     * 
+     * @param data Data to be inserted.
+     */
+    public void insert(E data) {
+        for (moveToFront(); (currNode
+                .getNext() != trailerNode); curseToNext()) {
+            if (currNode.getNext().getData().compareTo(data) > -1) {
+                // found existing data >= the data to be inserted
+                //
+                // create the node to be inserted
+                Node insertedNode = new Node();
+                insertedNode.setData(data);
+                // insert in the right position
+                insertedNode.setNext(currNode.getNext());
+                currNode.setNext(insertedNode);
+                ++count;
+                return;
+            }
+        }
+        // reached the end of the list
+        append(data);
+    }
+
+    /**
+     * Remove the first occurrence of data if exists.
+     * 
+     * @param  data Data to be removed from the list.
+     * 
+     * @return      True if removed, false otherwise.
+     */
+    public boolean remove(E data) {
+        //@formatter:off
+        for (moveToFront();
+                (currNode.getNext() != trailerNode);
+                curseToNext()) {
+            if (currNode.getNext().getData().equals(data)) {
+                currNode.setNext(currNode.getNext().getNext());
+                --count;
+                return true;
+            }
+        }
+        return false;
+        //@formatter:on
+    }
+
+    /**
+     * Pop the datum at the beginning of the list.
+     * 
+     * @return E data in the first node, null if the list is empty.
+     */
+    public E popFront() {
+        if (isEmpty()) {
+            return null;
+        }
+        moveToHead();
+        E data = yieldCurrNode();
+        remove(data);
+        return data;
+    }
+
+    /** Make the current node the head node. */
+    public void moveCurrNodeToFront() {
+        if (isEmpty()) {
+            return;
+        }
+        Node thisNode = currNode;
+        Node nextNode = currNode.getNext();
+        Node headNode = headerNode.getNext();
+
+        moveToFront();
+        currNode.setNext(thisNode);
+        thisNode.setNext(headNode);
+        curseToNext();
+        for (; !atEnd(); curseToNext()) {
+            if (currNode.getNext() == thisNode) {
+                currNode.setNext(nextNode);
+            }
+        }
+    }
+
+    /**
+     * Get the number of items in the list.
+     * 
+     * @return Number of items in the list.
+     */
+    public int getCount() {
+        return count;
+    }
+
+    /**
+     * Move the cursor node towards the end. Moving stops once the cursor hits
+     * the
+     * trailerNode.
+     */
+    public void curseToNext() {
+        if (currNode != trailerNode) {
+            currNode = currNode.getNext();
+        }
+    }
+
+    /**
+     * Get the data from the Node pointed by the currNode.
+     *
+     * @return Data in the current node pointed to by currNode.
+     */
+    public E yieldCurrNode() {
+        return currNode.getData();
+    }
+
+    /**
+     * Check if the list has a datum.
+     * 
+     * @param  data The datum being checked for.
+     * 
+     * @return      True if data exists in the list.
+     */
+    public boolean exists(E data) {
+        moveToHead();
+        while (!atEnd()) {
+            if (currNode.getData().equals(data)) {
+                return true;
+            }
+            curseToNext();
+        }
+        return false;
+    }
+
+    /**
+     * Yield the data stored at a given position when counted from the head.
+     *
+     * The counting is zero based. @note It is not an efficient method and
+     * should be avoided whenever possible.
+     * 
+     * @param  index The node index to be yielded. The head node's index is
+     *               zero.
+     * 
+     * @return       Data stored by the node at index position. Null if invalid
+     *               position is passed.
+     */
+    public E yieldIndex(int index) {
+        // check invalid index
+        if ((index >= getCount()) || (index < 0)) {
+            return null;
+        }
+        // we don't want to mutate the current node
+        // that was set on the list before this method
+        // was called
+        Node currentNodeBeforeProcessing = currNode;
+        moveToHead();
+        while (index > 0) {
+            --index;
+            curseToNext();
+        }
+        E data = yieldCurrNode();
+        // unmutate the current node
+        currNode = currentNodeBeforeProcessing;
+        return data;
+    }
+
+    /**
+     * Check if the current node is at the end of the list.
+     * 
+     * @return True if cursor is at the end of the list.
+     */
+    public boolean atEnd() {
+        return currNode == trailerNode;
+    }
+
+    /** Move the cursor to the head node. */
+    public void moveToHead() {
+        moveToFront();
+        curseToNext();
+    }
+
+    /** Move the cursor to the tail node. */
+    public void moveToTail() {
+        if (isEmpty()) {
+            return;
+        }
+        for (moveToHead(); currNode.getNext() != trailerNode; curseToNext()) {
+        }
+    }
+
+    /** Move the cursor node to the header node of the list. */
+    private void moveToFront() {
+        currNode = headerNode;
+    }
+
+    /** Move the cursor node to the trailer node of the list. */
+    private void moveToEnd() {
+        currNode = trailerNode;
+    }
+
+
+    /** The node data structure used in the LinkedList<E> class. */
+    private class Node {
+        /** Data stored by this Node. */
+        private E data;
+        /** Reference to the linked Node. */
+        private Node next;
+
+        /** Construct from null. */
+        Node() {
+            this.data = null;
+            this.next = null;
+        }
+
+        /** Getter for data. */
+        public E getData() {
+            return this.data;
+        }
+
+        /** Getter for next. */
+        public Node getNext() {
+            return this.next;
+        }
+
+        /** Setter for data. */
+        public void setData(E data) {
+            this.data = data;
+        }
+
+        /** Setter for next. */
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    } // class Node
+}
---
+++ AirControl/src/LinkedListTest.java
@@ -0,0 +1,187 @@
+import student.TestCase;
+
+/**
+ * Test the LinkedList class.
+ * 
+ * @author  Bimal Gaudel
+ * 
+ * @version Mar 24, 2020
+ */
+public class LinkedListTest extends TestCase {
+    /** A LinkedList<String> object used in the tests. */
+    private LinkedList<String> strList;
+
+    /** Sets up the tests that follow. */
+    public void setUp() {
+        strList = new LinkedList<String>();
+    }
+
+    /** Test the constructor. */
+    public void testConstructor() {
+        assertNotNull(strList);
+    }
+
+    /** Test the method append. */
+    public void testAppend() {
+
+        strList.append("Name");
+        strList.append("Class");
+        strList.moveToHead();
+        assertEquals(strList.yieldCurrNode(), "Name");
+        strList.curseToNext();
+        assertEquals(strList.yieldCurrNode(), "Class");
+    }
+
+    /** Test the remove method. */
+    public void testRemove() {
+        strList.append("Name");
+        strList.append("Class");
+
+        boolean removed = strList.remove("Name");
+        assertEquals(removed, true);
+
+        removed = strList.remove("Name");
+        assertEquals(removed, false);
+
+        removed = strList.remove("Class");
+        assertEquals(removed, true);
+    }
+
+    /** Test the method is empty. */
+    public void testIsEmpty() {
+        strList.append("Address");
+        assertEquals(strList.isEmpty(), false);
+        strList.remove("Address");
+        assertEquals(strList.isEmpty(), true);
+    }
+
+    /** Test the exists method. */
+    public void testExists() {
+        strList.append("Address");
+        assertEquals(strList.exists("Address"), true);
+        assertEquals(strList.exists("Class"), false);
+    }
+
+    /** Test getCount method. */
+    public void testGetCount() {
+        assertEquals(strList.getCount(), 0);
+        strList.append("Name");
+        assertEquals(strList.getCount(), 1);
+        strList.append("Class");
+        strList.append("Address");
+        assertEquals(strList.getCount(), 3);
+        strList.remove("Name");
+        assertEquals(strList.getCount(), 2);
+        strList.remove("Address");
+        assertEquals(strList.getCount(), 1);
+        strList.remove("Class");
+        assertEquals(strList.getCount(), 0);
+    }
+
+    /** Test yieldIndex method. */
+    public void testYieldIndex() {
+        strList.append("Name");
+        strList.append("Address");
+        strList.append("Year");
+
+        assertTrue(strList.yieldIndex(0).equals("Name"));
+        assertTrue(strList.yieldIndex(1).equals("Address"));
+        assertTrue(strList.yieldIndex(2).equals("Year"));
+
+        assertNull(strList.yieldIndex(-1));
+        assertNull(strList.yieldIndex(3));
+
+        // validate current node state is not mutated
+        strList.moveToHead();
+        strList.curseToNext(); // set curr node to "Address"
+        strList.yieldIndex(2);
+        assertTrue(strList.yieldCurrNode().equals("Address"));
+
+        strList = new LinkedList<String>();
+        assertNull(strList.yieldIndex(0));
+        assertNull(strList.yieldIndex(-1));
+        assertNull(strList.yieldIndex(1));
+    }
+
+    /** Test the insert method. */
+    public void testInsert() {
+        strList.insert("Name");
+        strList.moveToHead();
+        assertEquals(strList.yieldCurrNode(), "Name");
+
+        assertEquals(strList.getCount(), 1);
+
+        strList.insert("Naame");
+        strList.moveToHead();
+        assertEquals(strList.yieldCurrNode(), "Naame");
+
+        assertEquals(strList.getCount(), 2);
+
+        strList.insert("Class");
+        strList.insert("Day Job");
+        strList.insert("Address");
+        assertEquals(strList.getCount(), 5);
+        strList.moveToHead();
+        assertEquals(strList.yieldCurrNode(), "Address");
+    }
+
+    /** Test the popFront method. */
+    public void testPopFront() {
+        strList.append("Name");
+        strList.append("Class");
+        strList.append("Address");
+        assertEquals(strList.popFront(), "Name");
+        assertEquals(strList.popFront(), "Class");
+        assertEquals(strList.popFront(), "Address");
+        assertNull(strList.popFront());
+    }
+
+    /** Test the bubbleNodeToFront method. */
+    public void testBubbleNodeToFront() {
+        strList.append("Name");
+        strList.append("Class");
+        strList.append("Address");
+
+        strList.moveToHead();
+        strList.curseToNext();
+        assertEquals(strList.yieldCurrNode(), "Class");
+        // "Class" is now bubbled to the front
+        strList.moveCurrNodeToFront();
+
+        assertEquals(strList.popFront(), "Class");
+        assertEquals(strList.popFront(), "Name");
+        assertEquals(strList.popFront(), "Address");
+
+        // testing corner cases
+        strList.append("Name");
+        strList.moveCurrNodeToFront();
+        assertEquals(strList.popFront(), "Name");
+        assertNull(strList.popFront());
+        strList.moveCurrNodeToFront();
+        assertNull(strList.popFront());
+    }
+
+
+    /** Get code coverage for not executed statements. */
+    public void testGetCodeCoverage() {
+        LinkedList<String> myList = new LinkedList<String>();
+        myList.moveToTail();
+        myList.append("data");
+
+        myList.moveToHead();
+        myList.curseToNext();
+        myList.curseToNext();
+        myList.curseToNext();
+
+        assertTrue(myList.atEnd());
+
+        myList.append("more data");
+        myList.append("more more data");
+
+        myList.moveToHead();
+        myList.moveToTail();
+        myList.curseToNext();
+
+        assertTrue(myList.atEnd());
+    }
+}
